6886f016218473006b72451ccc10900c5d7441f5
Joshua <joshuawilliams7800@gmail.com>
2022-10-11 21:51:27 -0500

    fade to black added, buttons customized, code cleaned

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const number = document.querySelector(".number");
 const resetBtn = document.querySelector(".reset");
 const colorBtn = document.querySelector(".color");
 const classicBtn = document.querySelector(".classic");
+const fadeBtn = document.querySelector(".fade");
 
 let globalColor;
 
@@ -16,34 +17,61 @@ const createSquares = function (vol, option) {
   const totalSquares = vol.value * vol.value;
   const pixelsPerSqr = 400 / vol.value;
   const makeBlack = (e) => (e.target.style.backgroundColor = "black");
+  const randomColor = () => {
+    return Math.floor(Math.random() * 16777215).toString(16);
+  };
   for (i = 1; i <= totalSquares; i++) {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const square = document.createElement("div");
     square.className = "square";
     square.style.width = `${pixelsPerSqr}px`;
     square.style.height = `${pixelsPerSqr}px`;
-    if (option == "black") {
-      square.addEventListener("mousedown", makeBlack);
-      square.addEventListener("mouseover", (e) => {
-        if (e.buttons == 1) makeBlack(e);
-      });
-    } else if (option == "color") {
-      square.addEventListener("mousedown", (e) => {
-        e.target.style.backgroundColor = `#${randomColor}`;
-      });
-      square.addEventListener("mouseover", (e) => {
-        if (e.buttons == 1) {
-          e.target.style.backgroundColor = `#${randomColor}`;
-        }
-      });
+    switch (option) {
+      case "black":
+        square.addEventListener("mousedown", makeBlack);
+        square.addEventListener("mouseover", (e) => {
+          if (e.buttons == 1) makeBlack(e);
+        });
+        break;
+      case "color":
+        square.addEventListener("mousedown", (e) => {
+          e.target.style.backgroundColor = `#${randomColor()}`;
+        });
+        square.addEventListener("mouseover", (e) => {
+          if (e.buttons == 1) {
+            e.target.style.backgroundColor = `#${randomColor()}`;
+          }
+        });
+        break;
+      case "fade":
+        square.addEventListener("mousedown", (e) => {
+          if (e.target.style.backgroundColor == "") {
+            e.target.style.backgroundColor = "black";
+            e.target.style.opacity = 0.1;
+          } else {
+            e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
+          }
+        });
+        square.addEventListener("mouseover", (e) => {
+          if (e.buttons == 1 && e.target.style.backgroundColor == "") {
+            e.target.style.backgroundColor = "black";
+            e.target.style.opacity = 0.1;
+          } else if (e.buttons == 1) {
+            e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
+          }
+        });
     }
-
     board.append(square);
   }
 };
 
 createSquares(volume, "black");
 globalColor = "black";
+
+fadeBtn.addEventListener("click", (e) => {
+  globalColor = "fade";
+  reset();
+  createSquares(volume, globalColor);
+});
 
 colorBtn.addEventListener("click", (e) => {
   globalColor = "color";
